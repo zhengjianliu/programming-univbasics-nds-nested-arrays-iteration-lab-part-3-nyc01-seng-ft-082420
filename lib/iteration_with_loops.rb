de8992fa9d6b470552d56a73fd46197387d0ab6e
ZHENGJIAN LIU <comezheng@gmail.com>
2020-08-12 21:42:49 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,18 @@
+
 def join_nested_strings(src)
   # src will be an Array of Arrays of Strings and Integers
   # Combine all Strings present in the AoA into a single value and return it
+  row = 0
+  newArray = []
+  while row < src.size do
+    column = 0
+    while src[row].size > column do
+      if src[row][column].class == "string"
+        output << (src[row][column])
+      end
+      column+=1
+  end
+  row+=1
+  end
+  newArray.join(' ')
 end
